0c3d6617022b8fc2b5dd30c21ce7b72671b591bf
rgodlontonshaw <ryangs@methoddata.com>
2024-02-26 06:28:46 +0200

    rgs: getprofile and close functionality

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const moment = require('moment');
 const TelegramBot = require('node-telegram-bot-api');
 const admin = require('firebase-admin');
 const solanaWeb3 = require('@solana/web3.js');
+const { PublicKey } = require('@solana/web3.js');
 
 // Initialize Solana connection (adjust as necessary for your setup)
 const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('mainnet-beta'));
@@ -15,13 +16,16 @@ admin.initializeApp({
 const NewPairFetcher = require('./src/api/NewPairFetcher'); // Adjust the path as per your directory structure
 
 
-const pairFetcher =  new NewPairFetcher(process.env.DEXTOOLS_API_KEY);
+const pairFetcher = new NewPairFetcher(process.env.DEXTOOLS_API_KEY);
 
-const token = process.env.TELEGRAM_BOT_TOKEN;
+const token = '6820995483:AAGHg_jkICyGlzDBy0kAoWgcZPUzQmWhuxo';
 
 const bot = new TelegramBot(token, { polling: true });
 
 const db = admin.firestore();
+
+let publicKey;
+let solBalance;
 //
 // Function to create a new Solana wallet
 async function createSolanaWallet() {
@@ -33,23 +37,41 @@ async function createSolanaWallet() {
 // Function to fetch SOL balance
 async function getSolBalance(pubKey) {
   const balance = await connection.getBalance(new solanaWeb3.PublicKey(pubKey));
-  return balance / solanaWeb3.LAMPORTS_PER_SOL; 
+  return balance / solanaWeb3.LAMPORTS_PER_SOL;
 }
 
 // This function creates an inline keyboard layout for the start menu
 function getStartMenuKeyboard() {
   return {
     reply_markup: JSON.stringify({
-        inline_keyboard: [
-            [{ text: '⭐️ Strategies', callback_data: 'strategies' }, { text: '🎯 Quick Snipe and Trade', callback_data: 'quick_trade_sniper' }],
-            [{ text: '🤖 Rapid Snipe and Sell', callback_data: 'rapid_snipe' }, { text: '👤 Profile', callback_data: 'profile' }],
-            [{ text: '💼 Transfer Sol', callback_data: 'sol_transfer' }, { text: '🔄 Trades History', callback_data: 'trades_history' }],
-            [{ text: '🔥 New Pairs List', callback_data: 'new_pairs' }, { text: '🆕 New Pairs Volume List', callback_data: 'new_pairs_volume' }],
-            [{ text: '🔗 Referral System', callback_data: 'referral_system' }, { text: '⚙️ Settings', callback_data: 'settings' }],
-            [{ text: 'ℹ️ Help', callback_data: 'help' }, { text: '❌ Close', callback_data: 'close' }]
-        ],
+      inline_keyboard: [
+        [{ text: '⭐️ Strategies', callback_data: 'strategies' }, { text: '🎯 Quick Snipe and Trade', callback_data: 'quick_trade_sniper' }],
+        [{ text: '🤖 Rapid Snipe and Sell', callback_data: 'rapid_snipe' }, { text: '👤 Profile', callback_data: 'profile' }],
+        [{ text: '💼 Transfer Sol', callback_data: 'sol_transfer' }, { text: '🔄 Trades History', callback_data: 'trades_history' }],
+        [{ text: '🔥 New Pairs List', callback_data: 'new_pairs' }, { text: '🆕 New Pairs Volume List', callback_data: 'new_pairs_volume' }],
+        [{ text: '🔗 Referral System', callback_data: 'referral_system' }, { text: '⚙️ Settings', callback_data: 'settings' }],
+        [{ text: 'ℹ️ Help', callback_data: 'help' }, { text: '❌ Close', callback_data: 'close' }]
+      ],
     }),
-};
+  };
+}
+
+function getProfileMenuKeyboard() {
+  return {
+    reply_markup: JSON.stringify({
+      inline_keyboard: [
+        [{ text: '❌ Close', callback_data: 'close' }]
+      ],
+    }),
+  };
+}
+
+function clearStartMenuKeyboard() {
+  return {
+    reply_markup: {
+      inline_keyboard: []  // This effectively removes the keyboard
+    }
+  };
 }
 
 function encryptPrivateKey(privateKey, encryptionKey) {
@@ -74,7 +96,6 @@ bot.onText(/\/start/, async (msg) => {
   let userWalletDoc = db.collection('userWallets').doc(chatId);
   let doc = await userWalletDoc.get();
 
-  let publicKey; // Variable to hold the public key
   let decryptedPrivateKey;
 
   if (!doc.exists) {
@@ -82,29 +103,29 @@ bot.onText(/\/start/, async (msg) => {
     const newWallet = await createSolanaWallet();
     // Encrypt the private key before storing it
     const encryptedPrivateKey = encryptPrivateKey(newWallet.secretKey.toString('base64'), yourEncryptionKey);
-  
+
     await userWalletDoc.set({
       publicKey: newWallet.publicKey.toString(),
       privateKey: encryptedPrivateKey, // Store the encrypted private key
     });
-    
+
     publicKey = newWallet.publicKey;
   } else {
     // If the user already has a wallet, retrieve its public key from Firestore
     let walletData = doc.data();
     publicKey = new solanaWeb3.PublicKey(walletData.publicKey);
-   // decryptedPrivateKey = decryptPrivateKey(walletData.privateKey, yourEncryptionKey);
+    // decryptedPrivateKey = decryptPrivateKey(walletData.privateKey, yourEncryptionKey);
   }
 
   // Fetch SOL balance using the public key
   const solBalance = await getSolBalance(publicKey);
   const solscanUrl = `https://solscan.io/account/${publicKey.toString()}`; // Construct the Solscan URL
   const welcomeMessage = `Solana Wizard Bot: Your Gateway to Solana DeFi Professional Trading 🤖\n\n` +
-                         `Your Wallet Address\n` +
-                         `${publicKey.toString()}\n` +
-                         `Balance: ${solBalance.toFixed(6)} SOL\n\n` +
-                         `View on Explorer: ${solscanUrl}`; 
-  
+    `Your Wallet Address\n` +
+    `${publicKey.toString()}\n` +
+    `Balance: ${solBalance.toFixed(6)} SOL\n\n` +
+    `View on Explorer: ${solscanUrl}`;
+
 
   bot.sendMessage(chatId, welcomeMessage, getStartMenuKeyboard());
 });
@@ -112,10 +133,13 @@ bot.onText(/\/start/, async (msg) => {
 let transferState = {};
 
 // Handle callback queries from the inline keyboard
-bot.on('callback_query', (callbackQuery) => {
+bot.on('callback_query', async (callbackQuery) => {
   const message = callbackQuery.message;
   const data = callbackQuery.data;
-  const chatId = message.chat.id;
+  const action = callbackQuery.data;
+  const msg = callbackQuery.message;
+  const chatId = msg.chat.id;
+  const messageId = msg.message_id;
 
   // Reset transfer state if starting a new transfer or if any other button is pressed
   if (data !== 'input_amount' && data !== 'input_address') {
@@ -124,6 +148,7 @@ bot.on('callback_query', (callbackQuery) => {
 
   // Define the logic for each callback data
   switch (data) {
+
     case 'strategies':
       // Implement strategies functionality
       bot.sendMessage(chatId, 'Strategies functionality will be implemented soon.');
@@ -137,8 +162,10 @@ bot.on('callback_query', (callbackQuery) => {
       bot.sendMessage(chatId, 'Rapid Snipe and Sell functionality will be implemented soon.');
       break;
     case 'profile':
-      // Implement Profile functionality
-      bot.sendMessage(chatId, 'Profile functionality will be implemented soon.');
+  
+    bot.sendMessage(chatId, 'Viewing your profile...');
+    getProfile (chatId);
+
       break;
     case 'sol_transfer':
       transferState[chatId] = { stage: 'input_address_amount' };
@@ -146,20 +173,21 @@ bot.on('callback_query', (callbackQuery) => {
         'The address and amount are separated by commas.\n\n' +
         'Example:\n' +
         'EwR1MRLoXEQR8qTn1AF8ydwujqdMZVs53giNbDCxich,0.001');
-    break;
+      break;
     case 'trades_history':
       // Implement Trades History functionality
       bot.sendMessage(chatId, 'Trades History functionality will be implemented soon.');
       break;
     case 'new_pairs':
-   
+
       bot.sendMessage(chatId, "Starting to fetch new Solana token pairs...");
 
       // Start the polling process within the NewPairFetcher class
       pairFetcher.startPolling();
-      
+
       pairFetcher.on('newPair', (pair) => {
-        bot.sendMessage(chatId, `New Pair Detected!\nName: ${pair.tokenName}\nAddress: ${pair.tokenAddress}`);});
+        bot.sendMessage(chatId, `New Pair Detected!\nName: ${pair.tokenName}\nAddress: ${pair.tokenAddress}`);
+      });
       break;
     case 'new_pairs_volume':
       // Implement New Pairs Volume List functionality
@@ -178,8 +206,18 @@ bot.on('callback_query', (callbackQuery) => {
       bot.sendMessage(chatId, 'Help functionality will be implemented soon.');
       break;
     case 'close':
-      // Close the inline keyboard or exit the conversation
-      bot.sendMessage(chatId, 'Closing the menu.');
+      // await bot.editMessageReplyMarkup({ inline_keyboard: [] }, {
+      //   chat_id: chatId,
+      //   message_id: messageId
+      // }).catch((error) => {
+      //   console.error('Failed to edit message reply markup:', error);
+      // });
+      try {
+        let text = '';
+        await bot.deleteMessage(chatId, messageId);
+      } catch (error) {
+        console.error('Failed to "fade" message:', error);
+      }
       break;
     default:
       bot.sendMessage(chatId, 'Not sure what you want, try again.');
@@ -269,8 +307,7 @@ bot.onText(/\/rapidsnipe/, (msg) => {
 
 // Handle the /profile command
 bot.onText(/\/profile/, (msg) => {
-  // Your code to handle the profile command
-  bot.sendMessage(msg.chat.id, 'Viewing your profile...');
+  getProfile (msg.chat.id);
 });
 
 // Handle the /soltransfer command
@@ -294,9 +331,10 @@ bot.onText(/\/newpairs/, (msg) => {
 
   // Start the polling process within the NewPairFetcher class
   pairFetcher.startPolling();
-  
+
   pairFetcher.on('newPair', (pair) => {
-    bot.sendMessage(chatId, `New Pair Detected!\nName: ${pair.tokenName}\nAddress: ${pair.tokenAddress}`);});
+    bot.sendMessage(chatId, `New Pair Detected!\nName: ${pair.tokenName}\nAddress: ${pair.tokenAddress}`);
+  });
 });
 
 
@@ -325,5 +363,42 @@ bot.on('message', async (msg) => {
     // Handle other non-command messages or default case
     bot.sendMessage(chatId, "I didn't recognize that command. Try /start to see all available commands.");
   }
-transferState[chatId] = {};
+  transferState[chatId] = {};
 });
+
+async function getProfile (chatId) {
+  let userWalletDoc = db.collection('userWallets').doc(chatId.toString());
+  let doc = await userWalletDoc.get();
+
+  if (!doc.exists) {
+    // If the user doesn't have a wallet, create a new one and use its public key
+    const newWallet = await createSolanaWallet();
+    const encryptedPrivateKey = encryptPrivateKey(newWallet.secretKey.toString('base64'), yourEncryptionKey);
+
+    await userWalletDoc.set({
+      publicKey: newWallet.publicKey.toString(),
+      privateKey: encryptedPrivateKey, // Store the encrypted private key
+    });
+
+    publicKey = newWallet.publicKey;
+    solBalance = await getSolBalance(publicKey); // You should define getSolBalance() to fetch the balance
+  } else {
+    // If the user already has a wallet, retrieve its public key
+    let walletData = doc.data();
+    publicKey = new PublicKey(walletData.publicKey);
+    solBalance = await getSolBalance(publicKey); // You should define getSolBalance() to fetch the balance
+  }
+
+  const solscanUrl = `https://solscan.io/account/${publicKey.toString()}`; // Construct the Solscan URL
+
+  const profileMessage = `👤 PROFILE
+
+  --------------------------------------------------
+  Balance ◎: ${solBalance.toFixed(6)} SOL
+  --------------------------------------------------
+  Wallet Address: [${publicKey.toString()}](${solscanUrl})`;
+  await bot.sendMessage(chatId, profileMessage, {
+    parse_mode: 'Markdown',
+    ...getProfileMenuKeyboard() // Assuming getStartMenuKeyboard is defined and returns the keyboard object
+  });
+}
